ae82b6c20ae514d4abe938f040c77871dc7c3bbe
HYUNUK123 <141015078+HYUNUK123@users.noreply.github.com>
2023-12-26 10:07:53 +0900

    Update job4_fine_tuning.py

--- job4_fine_tuning.py
+++ job4_fine_tuning.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 client = OpenAI(
-    api_key="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")
+    api_key="")
 
 # # 생성된 데이터셋 파일을 openai 에 등록합니다.
 # client.files.create(
